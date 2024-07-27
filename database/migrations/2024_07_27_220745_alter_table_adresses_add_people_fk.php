44ea2f2f2c1c5c288a98d4dd4edef02f941aa56d
José Lucas <lukas0089@gmail.com>
2024-07-27 19:29:42 -0300

    feat: Migrations adicionando a coluna do endereço atual e a chave estrangeira para tabela pessoas na tabela endereços

---
+++ database/migrations/2024_07_27_220745_alter_table_adresses_add_people_fk.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('adresses', function(Blueprint $table){
+            $table->unsignedBigInteger('pessoa_id');
+            $table->foreign('pessoa_id', 'pessoa_fk')->references('id')->on('people');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('adresses', function(Blueprint $table){
+            $table->dropForeign('pessoa_fk');
+            $table->dropColumn('pessoa_id');
+        });
+    }
+};

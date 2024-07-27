eb3667f94c6a3076a8ef7120c24a68a5d7c9b1a8
José Lucas <lukas0089@gmail.com>
2024-07-27 19:30:38 -0300

    feat: Instalação e configuração do Inertia e Vuetify

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+import { Head, Link, router } from "@inertiajs/vue3";
+
+defineProps({
+    title: String,
+});
+</script>
+<template>
+    <v-responsive class="border rounded" max-height="300">
+        <v-app>
+            <v-app-bar title="Cadastro de Pessoas">
+                
+            </v-app-bar>
+            <slot></slot>
+        </v-app>
+    </v-responsive>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+</script>
+
+<template>
+    <AppLayout title="Consulta de Pessoas">
+        <v-main>
+            <v-container :title="title">
+                <v-container>
+                    <v-row>
+                        <v-text-field
+                            clearable
+                            label="Buscar"
+                            prepend-icon="mdi-magnify"
+                            variant="outlined"
+                        >
+                        </v-text-field>
+                    </v-row>
+                </v-container>
+            </v-container>
+        </v-main>
+    </AppLayout>
+</template>
